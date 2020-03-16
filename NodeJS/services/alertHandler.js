9c0e1a930593bc6fdf696a76919ff456b26d501f
Kasun Withana <kaswith4n@gmail.com>
2020-03-16 11:57:09 +0530

    dsdfdffd

--- NodeJS/services/alertHandler.js
+++ NodeJS/services/alertHandler.js
@@ -112,7 +112,130 @@ const handleAlerts = (trough, position, data)=>{
                 alerts.temperature.top.message = "Warning! Temperature of top has been above " + troughSettings.criticalTemp+" for more than 1 minute"
             }
         }
+
+        if(data.data.temperature.bottom <= troughSettings.criticalTemp){
+            lastLowtimes.trough1.start.temp.bottom = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.start.temp.bottom )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.start.temp.bottom)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.temperature.bottom.alertType =2;
+                alerts.temperature.bottom.message = "Warning! Temperature of bottom has been above " + troughSettings.criticalTemp+" for more than 1 minute"
+            }
+        }
+
+        if(data.data.bulbDiff.top <= 7 && data.data.bulbDiff.top>=2){
+            lastLowtimes.trough1.start.hum.top = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.start.hum.top )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.start.hum.top)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.bulbDiff.top.alertType =2;
+                if(data.data.bulbDiff.top > 7){
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of top has been above " + 7+" for more than 1 minute";
+                }else{
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of top has been below " + 2+" for more than 1 minute";
+                }
+            }
+
+        }
+
+        if(data.data.bulbDiff.bottom <= 7 && data.data.bulbDiff.bottom>=2){
+            lastLowtimes.trough1.start.hum.bottom = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.start.hum.bottom )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.start.hum.bottom)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.bulbDiff.bottom.alertType =2;
+                if(data.data.bulbDiff.bottom > 7){
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of bottom has been above " + 7+" for more than 1 minute";
+                }else{
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of bottom has been below " + 2+" for more than 1 minute";
+                }
+            }
+
+        }
+
+
+    }else if(position === "middle"){
+        console.log("In warning msg ctrl")
+        if(data.data.temperature.top <= troughSettings.criticalTemp){
+            lastLowtimes.trough1.middle.temp.top = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.middle.temp.top )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.middle.temp.top)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.temperature.top.alertType =2;
+                alerts.temperature.top.message = "Warning! Temperature of top has been above " + troughSettings.criticalTemp+" for more than 1 minute"
+            }
+        }
+
+        if(data.data.temperature.bottom <= troughSettings.criticalTemp){
+            lastLowtimes.trough1.middle.temp.bottom = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.middle.temp.bottom )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.middle.temp.bottom)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.temperature.bottom.alertType =2;
+                alerts.temperature.bottom.message = "Warning! Temperature of bottom has been above " + troughSettings.criticalTemp+" for more than 1 minute"
+            }
+        }
+
+        if(data.data.bulbDiff.top <= 7 && data.data.bulbDiff.top>=2){
+            lastLowtimes.trough1.middle.hum.top = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.middle.hum.top )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.middle.hum.top)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.bulbDiff.top.alertType =2;
+                if(data.data.bulbDiff.top > 7){
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of top has been above " + 7+" for more than 1 minute";
+                }else{
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of top has been below " + 2+" for more than 1 minute";
+                }
+            }
+
+        }
+
+        if(data.data.bulbDiff.bottom <= 7 && data.data.bulbDiff.bottom>=2){
+            lastLowtimes.trough1.middle.hum.bottom = Date.now();
+            console.log("low temp",  lastLowtimes.trough1.middle.hum.bottom )
+        }else{
+            var elapsedTime = (Date.now()-lastLowtimes.trough1.middle.hum.bottom)/60000;
+            console.log("ElapsedTime", elapsedTime);
+
+            if( elapsedTime>1){
+                console.log()
+                alerts.bulbDiff.bottom.alertType =2;
+                if(data.data.bulbDiff.bottom > 7){
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of bottom has been above " + 7+" for more than 1 minute";
+                }else{
+                    alerts.bulbDiff.top.message = "Warning! Bulb difference of bottom has been below " + 2+" for more than 1 minute";
+                }
+            }
+
+        }
+
     }
+
 
     
 

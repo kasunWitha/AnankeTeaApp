38af9510baf0ccb5a031b9782652e9d3a7f0f442
Kasun Withana <kaswith4n@gmail.com>
2020-06-13 01:35:27 +0530

    changes

--- NodeJS/websocket/wsClient.js
+++ NodeJS/websocket/wsClient.js
@@ -29,25 +29,27 @@ client  = mqtt.connect('mqtt://138.197.92.157:1883',options);
 
 client.on('connect', function () {
     console.log('mqtt connected ... ');
-    client.subscribe('a0315543/g8934028/d3605069/PUB');
-    client.subscribe('a0315543/g8934028/d1310166/PUB');
-    client.subscribe('a0315543/g8934028/d0003745/PUB');
-    client.subscribe('a0315543/g8934028/d6992123/PUB');
+     client.subscribe('a6140451/g9440826/d3605069/PUB');
+     client.subscribe('a6140451/g9440826/d1310166/PUB');
+     client.subscribe('a6140451/g9440826/d0003745/PUB');
+     client.subscribe('a6140451/g9440826/d6992123/PUB');
 });
 
 
 client.on('message', function (topic,message) {
+
+    //console.log(topic)
     
-    if(topic === 'a0315543/g8934028/d3605069/PUB'){
+    if(topic === 'a6140451/g9440826/d3605069/PUB'){
         console.log('device1', message.toString());
         messageParser.parseDeviceMessage1(message.toString());
-    }else if(topic === 'a0315543/g8934028/d1310166/PUB'){
+    }else if(topic === 'a6140451/g9440826/d1310166/PUB'){
         console.log('device2', message.toString());
         messageParser.parseDeviceMessage2(message.toString());
-    }else if(topic === 'a0315543/g8934028/d0003745/PUB'){
+    }else if(topic === 'a6140451/g9440826/d0003745/PUB'){
         console.log('device3', message.toString());
         messageParser.parseDeviceMessage3(message.toString());
-    }else if(topic === 'a0315543/g8934028/d6992123/PUB'){
+    }else if(topic === 'a6140451/g9440826/loc/PUB'){
         console.log('device4', message.toString());
         messageParser.parseDeviceMessage4(message.toString());
     }
--- dashboardApp/src/app/trough/trough.component.ts
+++ dashboardApp/src/app/trough/trough.component.ts
@@ -41,6 +41,8 @@ export class TroughComponent implements OnInit, OnChanges {
 
   flag1 = false;
   flag2 = false;
+  flag3 = false;
+  flag4 = false;
 
   bottomTemp = null;
   bottomHum = null;
@@ -50,10 +52,25 @@ export class TroughComponent implements OnInit, OnChanges {
   topHum2 = null;
   topBulbdiff2 = null;
 
+  topTemp3 = null;
+  topHum3 = null;
+  topBulbdiff3 = null;
+
+  topTemp4 = null;
+  topHum4 = null;
+  topBulbdiff4 = null;
+
   bottomTemp2 = null;
   bottomHum2 = null;
   bottomBulbdiff2 = null;
+
+  bottomTemp3 = null;
+  bottomHum3 = null;
+  bottomBulbdiff3 = null;
   
+  bottomTemp4 = null;
+  bottomHum4 = null;
+  bottomBulbdiff4 = null;
 
   tempTitle = "Temperature";
   humidTitle = "Humidity";
@@ -76,6 +93,24 @@ export class TroughComponent implements OnInit, OnChanges {
   humAlertTextTop2 = "Top:   No alerts";
   humAlertTextBottom2 = "Bottom: No alerts";
 
+  tempAlertStatusTop3 = 0;
+  tempAlertStatusBottom3 = 0;
+  humAlertStatusTop3 = 0;
+  humAlertStatusBottom3 = 0;
+  tempAlertTextTop3 = "Top:   No alerts";
+  tempAlertTextBottom3 = "Bottom:   No alerts";
+  humAlertTextTop3 = "Top:   No alerts";
+  humAlertTextBottom3 = "Bottom: No alerts";
+
+  tempAlertStatusTop4 = 0;
+  tempAlertStatusBottom4 = 0;
+  humAlertStatusTop4 = 0;
+  humAlertStatusBottom4 = 0;
+  tempAlertTextTop4 = "Top:   No alerts";
+  tempAlertTextBottom4 = "Bottom:   No alerts";
+  humAlertTextTop4 = "Top:   No alerts";
+  humAlertTextBottom4 = "Bottom: No alerts";
+
 
   constructor() { }
 
@@ -86,18 +121,17 @@ export class TroughComponent implements OnInit, OnChanges {
     
     try{
     //console.log("xxzzxzxz:",changes.dataArr.currentValue);
-    if(changes.dataArr.currentValue.position === "start"){
+    if(changes.dataArr.currentValue.trough === "trough1"){
     this.topTemp = changes.dataArr.currentValue.data.data.temperature.top;
     this.bottomTemp = changes.dataArr.currentValue.data.data.temperature.bottom;
     this.topHum = changes.dataArr.currentValue.data.data.humidity.top;
     this.bottomHum = changes.dataArr.currentValue.data.data.humidity.bottom;
     this.topBulbdiff = changes.dataArr.currentValue.data.data.bulbDiff.top;
     this.bottomBulbdiff = changes.dataArr.currentValue.data.data.bulbDiff.bottom; 
-
     this.handleAlerts(changes.dataArr.currentValue.alerts);
     this.flag1 = !this.flag1
 
-    }else if(changes.dataArr.currentValue.position === "middle"){
+    }else if(changes.dataArr.currentValue.trough === "trough2"){
       this.topTemp2 = changes.dataArr.currentValue.data.data.temperature.top;
       this.bottomTemp2 = changes.dataArr.currentValue.data.data.temperature.bottom;
       this.topHum2 = changes.dataArr.currentValue.data.data.humidity.top;
@@ -106,6 +140,26 @@ export class TroughComponent implements OnInit, OnChanges {
       this.bottomBulbdiff2 = changes.dataArr.currentValue.data.data.bulbDiff.bottom; 
       this.handleAlerts2(changes.dataArr.currentValue.alerts);
       this.flag2 = !this.flag2
+    }
+    else if(changes.dataArr.currentValue.trough === "trough3"){
+      this.topTemp3 = changes.dataArr.currentValue.data.data.temperature.top;
+      this.bottomTemp3 = changes.dataArr.currentValue.data.data.temperature.bottom;
+      this.topHum3 = changes.dataArr.currentValue.data.data.humidity.top;
+      this.bottomHum3 = changes.dataArr.currentValue.data.data.humidity.bottom;
+      this.topBulbdiff3 = changes.dataArr.currentValue.data.data.bulbDiff.top;
+      this.bottomBulbdiff3 = changes.dataArr.currentValue.data.data.bulbDiff.bottom; 
+      this.handleAlerts3(changes.dataArr.currentValue.alerts);
+      this.flag3 = !this.flag3
+
+    }else if(changes.dataArr.currentValue.trough === "trough4"){
+      this.topTemp4 = changes.dataArr.currentValue.data.data.temperature.top;
+      this.bottomTemp4 = changes.dataArr.currentValue.data.data.temperature.bottom;
+      this.topHum4 = changes.dataArr.currentValue.data.data.humidity.top;
+      this.bottomHum4 = changes.dataArr.currentValue.data.data.humidity.bottom;
+      this.topBulbdiff4 = changes.dataArr.currentValue.data.data.bulbDiff.top;
+      this.bottomBulbdiff4 = changes.dataArr.currentValue.data.data.bulbDiff.bottom; 
+      this.handleAlerts4(changes.dataArr.currentValue.alerts);
+      this.flag4 = !this.flag4
     }
 
     
@@ -143,6 +197,32 @@ handleAlerts2(alerts){
   this.humAlertTextTop2 = alerts.bulbDiff.top.message;
   this.humAlertStatusBottom2 = alerts.bulbDiff.top.alertType;
   this.humAlertTextBottom2 = alerts.bulbDiff.bottom.message;
+  
+}
+
+handleAlerts3(alerts){
+  this.tempAlertStatusTop3 = alerts.temperature.top.alertType;
+  this.tempAlertTextTop3 = alerts.temperature.top.message;
+  this,this.tempAlertStatusBottom3 = alerts.temperature.bottom.alertType;
+  this.tempAlertTextBottom3 = alerts.temperature.bottom.message;
+
+  this.humAlertStatusTop3 = alerts.bulbDiff.top.alertType;
+  this.humAlertTextTop3 = alerts.bulbDiff.top.message;
+  this.humAlertStatusBottom3 = alerts.bulbDiff.top.alertType;
+  this.humAlertTextBottom3 = alerts.bulbDiff.bottom.message;
+  
+}
+
+handleAlerts4(alerts){
+  this.tempAlertStatusTop4 = alerts.temperature.top.alertType;
+  this.tempAlertTextTop4 = alerts.temperature.top.message;
+  this,this.tempAlertStatusBottom4 = alerts.temperature.bottom.alertType;
+  this.tempAlertTextBottom4 = alerts.temperature.bottom.message;
+
+  this.humAlertStatusTop4 = alerts.bulbDiff.top.alertType;
+  this.humAlertTextTop4 = alerts.bulbDiff.top.message;
+  this.humAlertStatusBottom4 = alerts.bulbDiff.top.alertType;
+  this.humAlertTextBottom4 = alerts.bulbDiff.bottom.message;
   
 }
 

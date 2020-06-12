b94af8c6481fdfa0c94c5c975b216b21222efa07
Kasun Withana <kaswith4n@gmail.com>
2020-06-12 23:30:06 +0530

    changed to mqtt

--- NodeJS/services/messageParser.js
+++ NodeJS/services/messageParser.js
@@ -10,7 +10,7 @@ var { Trough } = require('../models/trough');
 
 
 
-const parseDeviceMessage = (message) =>{
+const parseDeviceMessage1 = (message) =>{
     try{
         try{
             var jsonMsg = JSON.parse(message);
@@ -19,10 +19,10 @@ const parseDeviceMessage = (message) =>{
             var tempArr = jsonMsg[deviceName].Temperature.replace(/\*C/g, "" ).replace().split(" ").map(Number);
             var humArr = jsonMsg[deviceName].Humidity.replace(/%/g, "" ).replace().split(" ").map(Number);
         }catch(err){
-            var inArr = parseNoJSONDeviceMsg(message);
+            var inArr = parseNoJSONDeviceMsg1(message);
             var tempArr = inArr[1];
             var humArr = inArr[0];
-            var deviceName = "ANANKENODE001";
+            var deviceName = "ANANKETEANODE005";
             console.log("real data");
         }
         //console.log("calc difference");
@@ -71,6 +71,188 @@ const parseDeviceMessage = (message) =>{
     }
 }
 
+const parseDeviceMessage2 = (message) =>{
+    try{
+        try{
+            var jsonMsg = JSON.parse(message);
+            var deviceName = Object.keys(jsonMsg)[0];
+            
+            var tempArr = jsonMsg[deviceName].Temperature.replace(/\*C/g, "" ).replace().split(" ").map(Number);
+            var humArr = jsonMsg[deviceName].Humidity.replace(/%/g, "" ).replace().split(" ").map(Number);
+        }catch(err){
+            var inArr = parseNoJSONDeviceMsg2(message);
+            var tempArr = inArr[1];
+            var humArr = inArr[0];
+            var deviceName = "ANANKETEANODE006";
+            console.log("real data");
+        }
+        //console.log("calc difference");
+        var bulbDifference = [calc.calculateBulbDiff(tempArr[0], humArr[0]), calc.calculateBulbDiff(tempArr[1], humArr[1])] ;
+        console.log("calc difference", deviceName);
+        deviceController.findByName(deviceName, (doc)=>{
+            console.log('deviceFound');
+            var device = new Device(doc);
+            console.log(device.trough.name);
+            console.log(getDate());
+            console.log(getHour());
+            console.log(humArr);
+            //var trough = new Trough()
+            var data = {
+                time : Date.now(),
+                data:{
+                    temperature:{
+                        top: tempArr[0],
+                        bottom: tempArr[1]
+                    },
+
+                    humidity:{
+                        top: humArr[0],
+                        bottom: humArr[1]
+                    },
+                    bulbDiff:{
+                        top: bulbDifference[0],
+                        bottom: bulbDifference[1]
+                    }
+                }
+            }
+
+            alerts = alertHandler.handleAlerts("trough2", "start", data);
+
+            broadcastHandler.broadcastMsg(device.trough.name, device.position, data, alerts);
+            console.log("MessAGE BROADCASTED");
+
+            troughController.addData(device.trough.name, getDate(), getHour(), device.position, data, (res)=>{
+                console.log(res);
+            });
+        });
+    }catch(err){
+        console.log("Inerr");
+        console.log(err);
+        
+    }
+}
+
+const parseDeviceMessage3 = (message) =>{
+    try{
+        try{
+            var jsonMsg = JSON.parse(message);
+            var deviceName = Object.keys(jsonMsg)[0];
+            
+            var tempArr = jsonMsg[deviceName].Temperature.replace(/\*C/g, "" ).replace().split(" ").map(Number);
+            var humArr = jsonMsg[deviceName].Humidity.replace(/%/g, "" ).replace().split(" ").map(Number);
+        }catch(err){
+            var inArr = parseNoJSONDeviceMsg3(message);
+            var tempArr = inArr[1];
+            var humArr = inArr[0];
+            var deviceName = "ANANKETEANODE007";
+            console.log("real data");
+        }
+        //console.log("calc difference");
+        var bulbDifference = [calc.calculateBulbDiff(tempArr[0], humArr[0]), calc.calculateBulbDiff(tempArr[1], humArr[1])] ;
+        console.log("calc difference", deviceName);
+        deviceController.findByName(deviceName, (doc)=>{
+            console.log('deviceFound');
+            var device = new Device(doc);
+            console.log(device.trough.name);
+            console.log(getDate());
+            console.log(getHour());
+            console.log(humArr);
+            //var trough = new Trough()
+            var data = {
+                time : Date.now(),
+                data:{
+                    temperature:{
+                        top: tempArr[0],
+                        bottom: tempArr[1]
+                    },
+
+                    humidity:{
+                        top: humArr[0],
+                        bottom: humArr[1]
+                    },
+                    bulbDiff:{
+                        top: bulbDifference[0],
+                        bottom: bulbDifference[1]
+                    }
+                }
+            }
+
+            alerts = alertHandler.handleAlerts("trough3", "start", data);
+
+            broadcastHandler.broadcastMsg(device.trough.name, device.position, data, alerts);
+            console.log("MessAGE BROADCASTED");
+
+            troughController.addData(device.trough.name, getDate(), getHour(), device.position, data, (res)=>{
+                console.log(res);
+            });
+        });
+    }catch(err){
+        console.log("Inerr");
+        console.log(err);
+        
+    }
+}
+
+const parseDeviceMessage4 = (message) =>{
+    try{
+        try{
+            var jsonMsg = JSON.parse(message);
+            var deviceName = Object.keys(jsonMsg)[0];
+            
+            var tempArr = jsonMsg[deviceName].Temperature.replace(/\*C/g, "" ).replace().split(" ").map(Number);
+            var humArr = jsonMsg[deviceName].Humidity.replace(/%/g, "" ).replace().split(" ").map(Number);
+        }catch(err){
+            var inArr = parseNoJSONDeviceMsg4(message);
+            var tempArr = inArr[1];
+            var humArr = inArr[0];
+            var deviceName = "ANANKETEANODE008";
+            console.log("real data");
+        }
+        //console.log("calc difference");
+        var bulbDifference = [calc.calculateBulbDiff(tempArr[0], humArr[0]), calc.calculateBulbDiff(tempArr[1], humArr[1])] ;
+        console.log("calc difference", deviceName);
+        deviceController.findByName(deviceName, (doc)=>{
+            console.log('deviceFound');
+            var device = new Device(doc);
+            console.log(device.trough.name);
+            console.log(getDate());
+            console.log(getHour());
+            console.log(humArr);
+            //var trough = new Trough()
+            var data = {
+                time : Date.now(),
+                data:{
+                    temperature:{
+                        top: tempArr[0],
+                        bottom: tempArr[1]
+                    },
+
+                    humidity:{
+                        top: humArr[0],
+                        bottom: humArr[1]
+                    },
+                    bulbDiff:{
+                        top: bulbDifference[0],
+                        bottom: bulbDifference[1]
+                    }
+                }
+            }
+
+            alerts = alertHandler.handleAlerts("trough4", "start", data);
+
+            broadcastHandler.broadcastMsg(device.trough.name, device.position, data, alerts);
+            console.log("MessAGE BROADCASTED");
+
+            troughController.addData(device.trough.name, getDate(), getHour(), device.position, data, (res)=>{
+                console.log(res);
+            });
+        });
+    }catch(err){
+        console.log("Inerr");
+        console.log(err);
+        
+    }
+}
 
 const getDate = ()=>{
     timestamp = new Date();
@@ -83,7 +265,7 @@ const getHour = ()=>{
 }
 
 
-const parseNoJSONDeviceMsg=(message)=>{
+const parseNoJSONDeviceMsg1=(message)=>{
     try{
 
         var editedMsg = message.replace(/{ANANKETEANODE001:/g, "").replace(/}/g, "").replace("[", "").replace("]", "")
@@ -108,5 +290,80 @@ const parseNoJSONDeviceMsg=(message)=>{
     
 }
 
+
+const parseNoJSONDeviceMsg2=(message)=>{
+    try{
+
+        var editedMsg = message.replace(/{ANANKETEANODE001:/g, "").replace(/}/g, "").replace("[", "").replace("]", "")
+        var arr = editedMsg.split(",");
+        var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+        var tempArray = arr[1].replace("Temperature :", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+        console.log("Array", arr, tempArray, humArray);
+        var outArr = [humArray.map(Number), tempArray.map(Number)];
+        
+    }catch (err){
+        console.log(err);
+        var outArr = [[0,0], [0,0]];
+
+    }
+
+    return outArr;
+
+   // var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+    //var tempArray = arr[1].replace("Temperature:", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+    
+
+    
+}
+
+const parseNoJSONDeviceMsg3=(message)=>{
+    try{
+
+        var editedMsg = message.replace(/{ANANKETEANODE001:/g, "").replace(/}/g, "").replace("[", "").replace("]", "")
+        var arr = editedMsg.split(",");
+        var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+        var tempArray = arr[1].replace("Temperature :", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+        console.log("Array", arr, tempArray, humArray);
+        var outArr = [humArray.map(Number), tempArray.map(Number)];
+        
+    }catch (err){
+        console.log(err);
+        var outArr = [[0,0], [0,0]];
+
+    }
+
+    return outArr;
+
+   // var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+    //var tempArray = arr[1].replace("Temperature:", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+    
+
+    
+}
+
+const parseNoJSONDeviceMsg4=(message)=>{
+    try{
+
+        var editedMsg = message.replace(/{ANANKETEANODE001:/g, "").replace(/}/g, "").replace("[", "").replace("]", "")
+        var arr = editedMsg.split(",");
+        var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+        var tempArray = arr[1].replace("Temperature :", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+        console.log("Array", arr, tempArray, humArray);
+        var outArr = [humArray.map(Number), tempArray.map(Number)];
+        
+    }catch (err){
+        console.log(err);
+        var outArr = [[0,0], [0,0]];
+
+    }
+
+    return outArr;
+
+   // var humArray = arr[0].replace("Humidity:", "").replace(/%/g, "").replace(/\t/g, "").trim().split(" ");
+    //var tempArray = arr[1].replace("Temperature:", "").replace(/\*C/g, "").replace(/\t/g, "").trim().split(" ");
+    
+
+    
+}
 
 module.exports = {parseDeviceMessage}
--- NodeJS/websocket/wsClient.js
+++ NodeJS/websocket/wsClient.js
@@ -1,15 +1,38 @@
-const WebSocket = require('ws');
+//const WebSocket = require('ws');
 
-const url = 'ws://138.197.92.157:2222';
+// const url = 'ws://138.197.92.157:2222';
 
-const messageParser = require('../services/messageParser');
+// const messageParser = require('../services/messageParser');
 
-const connection = new WebSocket(url);
+// const connection = new WebSocket(url);
 
-connection.onopen = ()=>{
-    console.log("websocket connected.");
-}
+// connection.onopen = ()=>{
+//     console.log("websocket connected.");
+// }
 
-connection.onmessage = (msg)=>{
-    messageParser.parseDeviceMessage(msg.data);
-}
+// connection.onmessage = (msg)=>{
+//     messageParser.parseDeviceMessage(msg.data);
+// }
+
+var mqtt = require('mqtt');
+
+var options = {
+    clientId:'123',
+    username: '123',
+    password: '123'
+};
+
+
+client  = mqtt.connect('mqtt://138.197.92.157:1883',options);
+
+client.on('connect', function () {
+    console.log('mqtt connected ... ');
+    client.subscribe('a0315543/g8934028/d5298846/PUB');
+    client.subscribe('a0315543/g8934028/d5298847/PUB');
+});
+
+
+client.on('message', function (topic,message) {
+                    
+    console.log(message.toString());
+});
